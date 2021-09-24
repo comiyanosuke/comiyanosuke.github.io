f7a93be4d1414b1ca5b7afee5dee39304fa18e5e
comiyanosuke <63603592+comiyanosuke@users.noreply.github.com>
2021-09-24 20:49:08 +0900

    Delete scriptcopy.js

--- js/scriptcopy.js
+++
@@ -1,9 +0,0 @@
-let mySwiper = new Swiper('.swiper-container', {
-  navigation: {  //ナビゲーションのオプション（矢印ボタンの要素を指定）
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  
-
-});
